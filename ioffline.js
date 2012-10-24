982cf207c0076f56795a67fc84373212e635c833
azrael <azrael@imatlas.com>
2012-10-24 21:03:39 +0800

    up
    Signed-off-by: azrael <azrael@imatlas.com>

--- ioffline.js
+++ ioffline.js
@@ -21,8 +21,13 @@ var pickupJs = function(url){
 
 var pickupCss = function(url, manifest){
 	var content = fs.readFileSync(url).toString();
-	var attr = 'manifest="' + manifest + '">'
-	content.replace(/(<html)([^>]*>)/i, '$1' + attr + '$2');
+	var maniReg = /<html.+manifest="?([^"]*)"?[^>]*>/i;
+	if(maniReg.test(content)){
+		// content = content.replace(maniReg, '$1');
+	}else{
+		var attr = ' manifest="' + manifest + '" '
+		content = content.replace(/(<html)([^>]*>)/i, '$1' + attr + '$2');
+	}
 	fs.writeFileSync(url, content);
 	var reg = /<link\s+.*?href="?([^"]+)"?[^>]+>/gi;
 	var attReg = /rel="?\bstylesheet\b"?/i;
@@ -37,6 +42,7 @@ var pickupCss = function(url, manifest){
 }
 
 var pickupImg = function(url){
+	url = url.split('?')[0];
 	console.log(url);
 	var content = fs.readFileSync(url).toString();
 	var reg = /url\("?([^")]+)"\)/gi;
@@ -82,14 +88,14 @@ exports.generate = function(configFile){
 			list.push(html);
 			// 收集 js
 			jss = pickupJs(html);
-			list.splice(list.length, 0, jss);
+			list = list.concat(jss);
 			// 收集 css
 			csss = pickupCss(html, manifest);
-			list.splice(list.length, 0, csss);
+			list = list.concat(csss);
 			// 收集 css 里面的图片
 			for(var j = 0, css; css = csss[j]; j++) {
 				imgs = pickupImg(css.replace(config.linkPrefix, ''));
-				list.splice(list.length, 0, imgs);
+				list = list.concat(imgs);
 			}
 		}
 		//创建 manifest 
--- test.js
+++
@@ -1,1 +0,0 @@
-require('./ioffline').generate('./config.json');
